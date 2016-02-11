2538cb5b83d050f6543d278a48338e9b590b057e
MaxSparkS <5611771@mail.ru>
2016-02-11 16:53:07 +0300

    JavaRush

--- src/com/javarush/test/level14/lesson08/bonus03/Singleton.java
+++ src/com/javarush/test/level14/lesson08/bonus03/Singleton.java
@@ -5,4 +5,13 @@ package com.javarush.test.level14.lesson08.bonus03;
  */
 public class Singleton
 {
+    static Singleton singleton = null;
+    private Singleton(){}
+    static Singleton getInstance(){
+        if (singleton==null){
+            singleton=new Singleton();
+        }
+        return singleton;
+    }
+
 }
