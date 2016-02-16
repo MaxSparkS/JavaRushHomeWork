4e69e581ce207c09fd213615afe95d970c9be448
MaxSparkS <5611771@mail.ru>
2016-02-16 12:32:38 +0300

    JavaRush

--- src/com/javarush/test/level15/lesson12/bonus01/Plane.java
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -5,6 +5,13 @@ package com.javarush.test.level15.lesson12.bonus01;
  */
 public class Plane implements Flyable
 {
+    int anInt;
+    public Plane(int anInt)
+    {
+        this.anInt=anInt;
+
+    }
+
     @Override
     public void fly()
     {
