114eb701d2edf8111adceba0c47204ad50d25b82
MaxSparkS <5611771@mail.ru>
2016-02-16 12:19:58 +0300

    JavaRush

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Helicopter.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by User on 16.02.16.
+ */
+public class Helicopter implements Flyable
+{
+    @Override
+    public void fly()
+    {
+
+    }
+}
---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by User on 16.02.16.
+ */
+public class Plane implements Flyable
+{
+    @Override
+    public void fly()
+    {
+
+    }
+}
